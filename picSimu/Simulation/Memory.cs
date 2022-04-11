089997d1edcac5bbe1236fdfe5db0981f802df18
S7uX <valerio.cocco@outlook.de>
2022-04-11 15:15:14 +0200

    add Memory.cs

--- picSimu/Simulation/Memory.cs
+++ picSimu/Simulation/Memory.cs
@@ -2,10 +2,6 @@ namespace picSimu.Simulation;
 
 public class Memory
 {
-    /*
-     * special registers
-     * 3: status
-     */
     private ushort[] register = new ushort[256];
 
     public bool BankSelect()
@@ -13,13 +9,13 @@ public class Memory
         return Lib.IsBitSet(register[3], 5);
     }
 
-    public ushort ReadRegister(byte adress)
+    public ushort ReadRegister(uint address)
     {
         if (!BankSelect())
         {
             // bank 0
-            adress |= 0 << 7;
-            if (0x30 >= adress)
+            address = address.SetBitTo0(7);
+            if (0x30 <= address || address == 7)
             {
                 return 0;
             }
@@ -27,17 +23,92 @@ public class Memory
         else
         {
             // bank 1
-            adress |= 1 << 7;
-            if (0xD0 >= adress)
+            address = address.SetBitTo1(7);
+            if (0xD0 <= address || address == 0x87)
             {
                 return 0;
             }
         }
 
-        return 0;
+        switch (address)
+        {
+            case 0: // Indirect addr
+            case 0x80:
+                return register[0];
+            case 2: // pcl
+            case 0x82:
+                return register[2];
+            case 3: // status
+            case 0x83:
+                return register[3];
+            case 4: // fsr
+            case 0x84:
+                return register[4];
+            case 0x0A: // pclath
+            case 0x8A:
+                return register[0x0A];
+            case 0x0B: // intcon
+            case 0X8B:
+                return register[0x0B];
+        }
+
+        return register[address];
     }
 
-    public void WriteRegister(byte adress)
+    public void WriteRegister(uint address, ushort value)
     {
+        if (!BankSelect())
+        {
+            // bank 0
+            address = address.SetBitTo0(7);
+            if (0x30 <= address || address == 7)
+            {
+                return;
+            }
+        }
+        else
+        {
+            // bank 1
+            address = address.SetBitTo1(7);
+            if (0xD0 <= address || address == 0x87)
+            {
+                return;
+            }
+        }
+
+        switch (address)
+        {
+            case 0: // Indirect addr
+            case 0x80:
+                register[0] = value;
+                register[0x80] = value;
+                return;
+            case 2: // pcl
+            case 0x82:
+                register[2] = value;
+                register[0x82] = value;
+                return;
+            case 3: // status
+            case 0x83:
+                register[3] = value;
+                return;
+            case 4: // fsr
+            case 0x84:
+                register[4] = value;
+                register[0X84] = value;
+                return;
+            case 0x0A: // pclath
+            case 0x8A:
+                register[0x0A] = value;
+                register[0x8A] = value;
+                return;
+            case 0x0B: // intcon
+            case 0X8B:
+                register[0x0B] = value;
+                register[0x8B] = value;
+                return;
+        }
+
+        register[address] = value;
     }
 }
