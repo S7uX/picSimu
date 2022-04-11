64f57c5cd7e9add75b083871bab3a6c17efa879d
Lachfrosch <daniel.rittershofer@gmx.de>
2022-04-11 13:21:54 +0200

    Removed old Memory

--- picSimu/Simulation/Pic.cs
+++ picSimu/Simulation/Pic.cs
@@ -7,30 +7,10 @@ public class Pic
     private uint wRegister = 0;
 
     private Instruction[] _programMemory;
-
-    // status register
-    public bool ZeroFlag;
-    public bool CarryFlag;
-    public bool DigitcarryFlag;
-    public bool IrpFlag;
-    public bool Rp1Flag;
-    public bool Rp0Flag;
-    public bool ToFlag;
-    public bool PdFlag;
+    public Instruction[] ProgramMemory => _programMemory;
 
     public Pic(Instruction[] programMemory)
     {
-        _programMemory = programMemory;
-        
-        wRegister = wRegister;
-        
-        IrpFlag = false;
-        Rp1Flag = false;
-        Rp0Flag = false;
-        ToFlag = true;
-        PdFlag = true;
-        ZeroFlag = true;
-        CarryFlag = true;
-        DigitcarryFlag = true;
+        _programMemory = new Instruction[1024];
     }
 }
