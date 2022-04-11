5a4941ba82db853c1e9df108b9f7e3ff5c2086c0
S7uX <valerio.cocco@outlook.de>
2022-04-11 15:15:24 +0200

    add bit methods

--- picSimu/Simulation/Lib.cs
+++ picSimu/Simulation/Lib.cs
@@ -6,4 +6,28 @@ public static class Lib
     {
         return (b & (1 << pos)) != 0;
     }
+    
+    public static int SetBitTo1(this int value, int position)
+    {
+        // Set a bit at position to 1.
+        return value |= (1 << position);
+    }
+    
+    public static int SetBitTo0(this int value, int position)
+    {
+        // Set a bit at position to 0.
+        return value & ~(1 << position);
+    }
+    
+    public static uint SetBitTo1(this uint value, int position)
+    {
+        // Set a bit at position to 1.
+        return value |= (1u << position);
+    }
+    
+    public static uint SetBitTo0(this uint value, int position)
+    {
+        // Set a bit at position to 0.
+        return value & ~(1u << position);
+    }
 }
