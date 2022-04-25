229a7329c7e0c757e68e4a0ff79cdac90619a480
Lachfrosch <daniel.rittershofer@gmx.de>
2022-04-25 16:52:02 +0200

    Implemented Program 5

--- picSimu/Simulation/Instructions/BitOrientedInstructions/BCF.cs
+++ picSimu/Simulation/Instructions/BitOrientedInstructions/BCF.cs
@@ -4,7 +4,13 @@ public class BCF : BitOrientedInstruction
 {
     public override int Execute()
     {
-        throw new NotImplementedException();
+        var val = _pic.Memory.ReadRegister(f);
+
+        val = val.SetBitTo0(b);
+
+        _pic.Memory.WriteRegister(f, val);
+        _pic.Programmcounter++;
+        return 0;
     }
 
     public BCF(string binaryString, Pic pic) : base(binaryString, pic)
