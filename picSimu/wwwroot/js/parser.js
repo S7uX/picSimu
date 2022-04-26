a65b6c6e9f206c4391af746d93d02c921d9a3530
S7uX <valerio.cocco@outlook.de>
2022-04-26 09:58:16 +0200

    gui: line highlighting

--- picSimu/wwwroot/js/parser.js
+++ picSimu/wwwroot/js/parser.js
@@ -7,16 +7,21 @@ export async function parsePic(sourceCode) {
     const parser = new Parser;
     const PicLang = await Parser.Language.load("wasm/tree-sitter-pic.wasm")
     parser.setLanguage(PicLang);
-    
+
     const tree = parser.parse(sourceCode);
     console.log("syntax tree:");
     console.log(tree.rootNode);
 
+    let instructionCount = 0;
     for (const row of tree.rootNode.children) {
         let rowLength = 0;
         if (row.type === "row") {
             const rowSpan = document.createElement('span');
             for (const rowElement of row.children) {
+                if (rowElement.type === "instruction") {
+                    rowSpan.id = "instruction-" + instructionCount;
+                    instructionCount++;
+                }
                 // console.log(row.text)
                 const whitespaceLength = rowElement.startPosition.column - rowLength;
                 rowSpan.insertAdjacentText("beforeend", " ".repeat(rowElement.startPosition.column - rowLength));
@@ -24,12 +29,18 @@ export async function parsePic(sourceCode) {
                 rowSpan.insertAdjacentHTML("beforeend", elementHtml)
                 rowLength += whitespaceLength + rowElement.text.length
             }
-            rowSpan.insertAdjacentText("beforeend", "\n");  
+            rowSpan.classList.add("code-line");
+            rowSpan.insertAdjacentText("beforeend", "\n");
             codeBlock.appendChild(rowSpan);
         }
     }
 
     document.getElementById("code-block").replaceChildren(codeBlock);
+
+    const firstInstruction = document.getElementById("instruction-0");
+    if(firstInstruction !== null){
+        firstInstruction.classList.add("code-line-highlight");
+    }
 }
 
 export async function getInstructionCodes(sourceCode) {
@@ -55,4 +66,11 @@ export async function getInstructionCodes(sourceCode) {
     console.log("parsed instruction codes " + returnString);
 
     return returnString.slice(0, -1); // remove last comma
+}
+
+export function highlightCodeLine(programCounter) {
+    // remove previous highlighting
+    document.getElementsByClassName("code-line-highlight")[0].classList.remove("code-line-highlight");
+    // highlight new line
+    document.getElementById("instruction-" + programCounter).classList.add("code-line-highlight");
 }
