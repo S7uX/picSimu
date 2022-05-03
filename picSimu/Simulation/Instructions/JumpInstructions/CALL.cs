c3a93d335f311e528122c444670aad9d362f7e96
Lachfrosch <daniel.rittershofer@gmx.de>
2022-05-03 14:51:46 +0200

    fixed a little oopsy woopsy UwU

--- picSimu/Simulation/Instructions/JumpInstructions/CALL.cs
+++ picSimu/Simulation/Instructions/JumpInstructions/CALL.cs
@@ -9,9 +9,8 @@ public class CALL : JumpInstruction
     public override int Execute()
     {
         _pic.IncreaseProgramCounter();
+        _pic.Stack.Push(_pic.Memory.ReadRegister(2));
         _pic.IncreaseProgramCounter();
-        
-        _pic.Stack.Push(_pic.Memory.ReadRegister(2) + 1);
         _pic.Memory.WriteRegister(2, k);
         
         return 0;
